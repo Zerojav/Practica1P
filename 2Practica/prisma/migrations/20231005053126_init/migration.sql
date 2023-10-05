d84ff39be48f0fc9a3ca0a2745af27108ed12dc1
Zerojav <javiermero8@gmail.com>
2023-10-05 00:38:25 -0500

    2Practica

---
+++ 2Practica/prisma/migrations/20231005053126_init/migration.sql
@@ -0,0 +1,58 @@
+/*
+  Warnings:
+
+  - You are about to drop the `OptionsModel` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `UserModel` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "OptionsModel" DROP CONSTRAINT "OptionsModel_userId_fkey";
+
+-- DropTable
+DROP TABLE "OptionsModel";
+
+-- DropTable
+DROP TABLE "UserModel";
+
+-- CreateTable
+CREATE TABLE "Cliente" (
+    "id" SERIAL NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "telefono" INTEGER NOT NULL,
+    "direccion" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "Cliente_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Repartidor" (
+    "id" SERIAL NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "telefono" INTEGER NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "Repartidor_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Pedido" (
+    "numPedido" SERIAL NOT NULL,
+    "clienteId" INTEGER NOT NULL,
+    "repartidorId" INTEGER NOT NULL,
+    "fechaPedido" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Pedido_pkey" PRIMARY KEY ("numPedido")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Cliente_email_key" ON "Cliente"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Repartidor_email_key" ON "Repartidor"("email");
+
+-- AddForeignKey
+ALTER TABLE "Pedido" ADD CONSTRAINT "Pedido_clienteId_fkey" FOREIGN KEY ("clienteId") REFERENCES "Cliente"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Pedido" ADD CONSTRAINT "Pedido_repartidorId_fkey" FOREIGN KEY ("repartidorId") REFERENCES "Repartidor"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
